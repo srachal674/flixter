4ecd8d2be2bc8525daa59faee95b990589ed07ba
Shannon Rachal <srachal674@gmail.com>
2019-08-15 03:08:17 +0000

    Updated admin form

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -9,7 +9,7 @@ class Instructor::CoursesController < ApplicationController
   def create
     @course = current_user.courses.create(course_params)
     if @course.valid?
-      redirect_to instructor_course_path(@course)
+      redirect_to instructor_course_path(@courses)
     else
       render :new, status: :unprocessable_entity
     end
