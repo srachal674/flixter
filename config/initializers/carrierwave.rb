25a9dfc0c809c899f55f20b4092578a45ddf1e94
Shannon Rachal <srachal674@gmail.com>
2019-08-21 02:23:47 +0000

    Added .fetch to carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,9 +5,9 @@ CarrierWave.configure do |config|
   config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                   # required
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY'],   # required
-    aws_secret_access_key: ENV['AWS_SECRET_KEY'],   # required
+    aws_access_key_id:     ENV.fetch['AWS_ACCESS_KEY'],   # required
+    aws_secret_access_key: ENV.fetch['AWS_SECRET_KEY'],   # required
   }
 
-  config.fog_directory =   ENV['AWS_BUCKET']        # required
+  config.fog_directory =   ENV.fetch['AWS_BUCKET']        # required
 end
