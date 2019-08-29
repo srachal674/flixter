dae586fecd7006bb8920eacad664e2cfe3a89970
srachal674 <srachal674@gmail.com>
2019-08-29 01:46:03 +0000

    Added aws and carrierwave for video lesson 24

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                   # required
-  config.fog_public = false
-  config.fog_credentials = {
-    provider:              'AWS',                   # required
-    aws_access_key_id:     ENV.fetch('AWS_ACCESS_KEY'),   # required
-    aws_secret_access_key: ENV.fetch('AWS_SECRET_KEY'),   # required
-  }
+  config.storage = :aws                   # required
+  config.aws_bucket = ENV["AWS_BUCKET"]
+  config.aws_acl = "public-read"
 
-  config.fog_directory =   ENV.fetch('AWS_BUCKET')        # required
+  config.aws_credentials = {
+    access_key_id:     ENV.fetch["AWS_ACCESS_KEY"],   # required
+    secret_access_key: ENV.fetch["AWS_SECRET_KEY"],   # required
+    region:            ENV["AWS_REGION"]
+  }
 end
