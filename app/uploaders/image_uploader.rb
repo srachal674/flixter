f6ce1952ab2e76f7ca460306402a9cc127343330
srachal674 <srachal674@gmail.com>
2019-09-01 21:03:16 +0000

    Working on Lesson 24

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
-  storage :fog
+  storage :aws
 
   # A bunch of comments
 
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,13 @@
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
-  config.storage = :aws               #required
-  # config.aws_acl = false
-  config.aws_credentials = {
-    provider:               'AWS',                  #required
-    aws_access_key_id:      ENV["AWS_ACCESS_KEY"],   #required
+  config.storage = :aws 
+  config.aws_bucket = ENV["AWS_BUCKET"]             
+  config.aws_acl = "public-read"
+
+  config.aws_credentials = {           
+    aws_access_key_id:      ENV["AWS_ACCESS_KEY"],   
     aws_secret_access_key:  ENV["AWS_SECRET_KEY"],
+    region:                 ENV["AWS_REGION"]
   }
-  config.aws_bucket = ENV["AWS_BUCKET"]
 end
