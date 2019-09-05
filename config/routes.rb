244a4ec3cdd7572ffd6b442737e85685bb6f9222
Shannon Rachal <srachal674@gmail.com>
2019-09-05 00:14:58 +0000

    Lesson 27 done - Enroll button active

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-  resources :courses, only: [:index, :show]do
+  resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
 end
   resources :lessons, only: [:show]
