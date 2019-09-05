83bd9a6d6ae57957473ec8833a4d711489764556
Shannon Rachal <srachal674@gmail.com>
2019-09-05 05:50:56 +0000

    Working on Lesson 28 changed course to lesson in lesson controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,23 +1,23 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course, only: [:show]
+  before_action :require_authorized_for_current_lesson, only: [:show]
 
   def show
   end
     
     private
     def require_authorized_for_current_course
-      if current_course.user != current_user
-        render redirect_to "Sign in"
+      if current_lesson.user != current_user
+        render redirect_to current_lesson
       end
     end
   
-    helper_method :current_course
-    def current_course
-      @current_course ||= Course.find(params[:id])
+    helper_method :current_lesson
+    def current_lesson
+      @current_lesson ||= Lesson.find(params[:id])
     end
   
-    def course_params
+    def lesson_params
       params.require(:course).permit(:title, :description, :image, :cost)
     end
 
