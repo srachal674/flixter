dfa7830cdb2b477d3c84a76b3a4508f85ef46424
Shannon Rachal <srachal674@gmail.com>
2019-09-05 00:25:26 +0000

    Working on Lesson 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,24 @@
 class LessonsController < ApplicationController
-    def show
-    end
-  
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
+  def show
+  end
     
     private
-  
-    helper_method :current_lesson
-    def current_lesson
-      @current_lesson ||= Lesson.find(params[:id])
+    def require_authorized_for_current_course
+      if current_course.user != current_user
+        render redirect_to "Sign in"
+      end
     end
-  end
+  
+    helper_method :current_course
+    def current_course
+      @current_course ||= Course.find(params[:id])
+    end
+  
+    def course_params
+      params.require(:course).permit(:title, :description, :image, :cost)
+    end
+
+end
