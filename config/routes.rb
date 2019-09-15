460a9865dce28c81e070f0e6ecb0053de3791310
Shannon Rachal <srachal674@gmail.com>
2019-09-15 20:54:33 +0000

    Finished Lesson 31

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
-end
+  end
   resources :lessons, only: [:show]
   namespace :instructor do
+    resources :lessons, only: [:update]
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
