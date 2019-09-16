6dac3e6f019942a8808dca66372bd0d813587fac
Shannon Rachal <srachal674@gmail.com>
2019-09-16 00:49:31 +0000

    Added Lesson 30 for Section sort

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_225634) do
+ActiveRecord::Schema.define(version: 2019_09_15_210431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,10 +52,9 @@ ActiveRecord::Schema.define(version: 2019_09_14_225634) do
     t.integer "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "row_order"
     t.index ["course_id"], name: "index_sections_on_course_id"
-  end
-
-  create_table "structures", force: :cascade do |t|
+    t.index ["row_order"], name: "index_sections_on_row_order"
   end
 
   create_table "users", force: :cascade do |t|
