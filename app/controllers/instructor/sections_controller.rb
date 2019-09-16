19bd7ac94cb769c5bbe7b92b567e4a702d9a61b1
Shannon Rachal <srachal674@gmail.com>
2019-09-16 01:55:49 +0000

    Added drag and drop code for Sections Lesson 31v2 not working

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:new, :create]
+  before_action :require_authorized_for_current_section, only: [:update]
 
   def new
     @section = Section.new
@@ -10,12 +11,27 @@ class Instructor::SectionsController < ApplicationController
     @section = current_course.sections.create(section_params)
     redirect_to instructor_course_path(current_course)
   end
+  
+  def update
+    current_section.update_attributes(section_params)
+    render plain: 'updated!'
+  end
 
   private
 
+  def current_section
+    @current_section ||= Section.find(params[:id])
+  end
+
   def require_authorized_for_current_course
     if current_course.user != current_user
-      render plain: "Unauthorized", status: :unauthorized
+      render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+  def require_authorized_for_current_section
+    if current_section.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
@@ -25,6 +41,6 @@ class Instructor::SectionsController < ApplicationController
   end
 
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
 end
