75270d86154f4e3023af37308d34157cc58d18c3
Shannon Rachal <srachal674@gmail.com>
2019-09-17 04:11:31 +0000

    Working on Lesson 32

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -36,8 +36,12 @@ class Instructor::SectionsController < ApplicationController
   end
 
   helper_method :current_course
-  def current_course
-    @current_course ||= Course.find(params[:course_id])
+    def current_course
+      if params[:course_id]
+        @current_course ||= Course.find(params[:course_id])
+      else
+        current_section.course
+      end
   end
 
   def section_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
+
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [:update]
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
+    
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
