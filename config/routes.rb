85313f945320111b99fbb9343f0b14db8fb56779
Shannon Rachal <srachal674@gmail.com>
2019-10-01 00:05:30 +0000

    Fixed the create a section connection. Working on the create a lesson connection:

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,10 @@ Rails.application.routes.draw do
 
   resources :lessons, only: [:show]
   namespace :instructor do
+    resources :sections, only: [:update]
     resources :lessons, only: [:update]
-    resources :sections, only: [:update] do
-      resources :lessons, only: [:new, :create]
+    resources :sections, only: [] do
+      resources :lessons, only: [:create]
     end
     
     resources :courses, only: [:new, :create, :show] do
